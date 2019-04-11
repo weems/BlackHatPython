9b21978fbd9fbe085732fd768afd0ecfc28025f5
Michael Weems <weems@weems.io>
2019-04-11 11:43:40 -0500

    update netcat methods

--- bhpnet.py
+++ bhpnet.py
@@ -1,4 +1,7 @@
+import sys
+
 # define some global variables
+
 listen = False
 command = False
 upload = False
@@ -13,4 +16,25 @@ def usage():
     print("Usage: bhpnet.py -t target_host -p port")
     print("-l --listen                - listen on [host]:[port] for\n incoming connections.")
     print("-e --execute=file_to_run   - execute the given file upon\n receiving a connection")
+    print("-c --command               - initialize a command shell")
+    print("-u --upload=destination   - upon receiving connection upload a\n file and write to [destination]")
     print("")
+    print("")
+    print("Examples:")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -c")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -u=c:\\target.exe")
+    print("bhpnet.py -t 192.168.0.1 -p 5555 -l -e=\"cat /etc/passwd\"")
+    print("echo 'ABCDEFGHI' | ./bhpnet.py -t 192.168.11.12 -p 135")
+    sys.exit(0)
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+    # read the commandline options
