6ff9420fc97c4f764a56b1a84af3fb2d896eefea
Michael Weems <weems@weems.io>
2019-04-10 23:41:43 -0500

    Added tcp and udp client[s]

--- tcp_client.py
+++ tcp_client.py
@@ -7,3 +7,13 @@ target_port = 80
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # connect the client
+client.connect((target_host, target_port))
+
+# send some data
+data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n".encode()
+client.send(data)
+
+# receive some data
+response = client.recv(4096)
+
+print(response)
